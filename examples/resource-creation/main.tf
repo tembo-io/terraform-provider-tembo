b5d479afdcd0028ec4d3f6c77812cd2acce27e69
Adarsh Shah <github@shahadarsh.com>
2023-08-22 11:20:53 -0400

    sets cluster state for cluster resource & implements read method for it (#5)

--- examples/resource-creation/main.tf
+++ examples/resource-creation/main.tf
@@ -10,10 +10,14 @@ provider "tembo" {}
 
 resource "tembo_cluster" "adarsh_db" {
   cluster_name    = "adarsh-db"
-  organization_id = "test"
+  organization_id = "org_30testfkw8WUVpJc6Zuhc"
   cpu             = "4"
   stack           = "Standard"
   environment     = "dev"
   memory          = "8Gi"
   storage         = "10Gi"
+}
+
+output "cluster" {
+  value = tembo_cluster.adarsh_db
 }
