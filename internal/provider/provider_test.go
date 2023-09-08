6be920c552048dc84b1aeddbee221d1a01f0826c
Adarsh Shah <github@shahadarsh.com>
2023-09-07 21:29:22 -0400

    compares fields to see if update worked instead of using status

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -31,11 +31,3 @@ provider "tembo" {
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"tembo": providerserver.NewProtocol6WithError(New("test")()),
 }
-
-func testAccPreCheck() {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
-	os.Setenv("TEMBO_HOST", "test_host")
-	os.Setenv("TEMBO_ACCESS_TOKEN", "test_access_token")
-}
